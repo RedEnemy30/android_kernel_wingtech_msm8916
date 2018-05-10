c69729c8cbc097b8aad917b20e8cc13020db9eb5
YudiWidiyanto <yudiwidiyanto7@gmail.com>
2018-05-10 18:37:37 +0700

    zpx.sh: update defconfig

--- zpx.sh
+++ zpx.sh
@@ -39,7 +39,7 @@ yellow='\033[0;33m'
 red='\033[0;31m'
 nocol='\033[0m'
 ## should be preset in arch/arm/configs of kernel-code
-KERNEL_DEFCONFIG=lineageos_wt88047_defconfig
+KERNEL_DEFCONFIG=excalibur_defconfig
 
 ## make jobs
 MAKE_JOBS=$( nproc --all )
